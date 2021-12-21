2f46c28a25d663df5d8c940c5df5179c64de88c4
Girish21 <girishkumarv21@gmail.com>
2021-12-21 20:51:17 +0530

    chore: fix and initialise three

---
+++ app/components/animation/index.jsx
@@ -0,0 +1,15 @@
+import { Suspense } from "react";
+import { ClientOnly } from "remix-utils";
+import AnimationImpl from "./animation.client";
+
+const Animation = () => {
+  return (
+    <ClientOnly>
+      <Suspense fallback={null}>
+        <AnimationImpl />
+      </Suspense>
+    </ClientOnly>
+  );
+};
+
+export default Animation;
